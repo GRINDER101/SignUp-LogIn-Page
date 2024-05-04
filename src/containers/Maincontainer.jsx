5351934aeac47573e92e410df4cf51e0f1792606
Rittik Saha <rittiksaha7044@gmail.com>
2024-05-05 02:22:44 +0530

    first commit

--- src/containers/Maincontainer.jsx
+++ src/containers/Maincontainer.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import "./maincontainer.css"
-import Newcontainer from '../Pages/Newcontainer/Newcontainer'
-import Intialpage from '../Pages/Initialpage/Intialpage'
 
-const Maincontainer = ({ openIntro }) => {
+import React, { useEffect, useState } from 'react'
+import "./maincontainer.css"
+import Newcontainer from "../Pages/Newcontainer/Newcontainer"
+import Intialpage from "../Pages/Initialpage/Intialpage"
+
+const Maincontainer = () => {
+    const [openIntro, setOpenIntro] = useState(false)
+    const [iconVisible, setIsIconVisible] = useState(true)
+
+    useEffect(() => {
+        setInterval(() => {
+            setIsIconVisible(false)
+        }, 10000)
+    }, [iconVisible])
     return (
 
         <div >
             {openIntro ? <Newcontainer /> : <Intialpage />}
+            {iconVisible ?
+                <div id="show-intro" onClick={() => { setOpenIntro(true); setIsIconVisible(false) }}>
+                    <i className="ri-arrow-left-double-line"></i>
+                </div> : <></>}
         </div>
     )
 }
