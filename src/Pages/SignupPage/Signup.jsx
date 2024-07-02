1884f32e44ba348868abbaaf2fcb4611af83b649
Rittik Saha <89324459+GRINDER101@users.noreply.github.com>
2024-07-02 21:00:48 +0100

    Update Signup.jsx

--- src/Pages/SignupPage/Signup.jsx
+++ src/Pages/SignupPage/Signup.jsx
@@ -1,51 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./signup.css"
-import Login from '../LoginPage/Login';
 
-const Signup = () => {
-    const [userSignedin, setuserSignedin] = useState(false)
-    const [signupconfirmation, IssignupConfirmed] = useState(false)
-    const [userdetails, setUserdetails] = useState({
-        fname: '',
-        lname: '',
-        email: '',
-        confirmedEmail: '',
-        password: '',
-        confirmedPassword: '',
-    });
-    function HandleSubmit(e) {
-        e.preventDefault();
-        if (!userdetails.fname || !userdetails.lname || !userdetails.email || !userdetails.confirmedEmail || !userdetails.confirmedPassword) {
-            alert("fill all the details! ")
-        }
-        else if (userdetails.password.length < 8) {
-            setUserdetails({ ...userdetails, password: '', confirmedPassword: '' })
-            document.getElementById('password').setAttribute('placeholder', 'password need to be 8 characters long')
-            document.getElementById('password').classList.add('error');
-        }
-        else if (userdetails.email !== userdetails.confirmedEmail && userdetails.password !== userdetails.confirmedPassword) {
-            setUserdetails({ ...userdetails, confirmedEmail: '', confirmedPassword: '' })
-            document.getElementById('confirmedEmail').setAttribute('placeholder', 'wrong email-id provided, confirm your email again')
-            document.getElementById('confirmedEmail').classList.add('error');
-            document.getElementById('confirmedPassword').setAttribute('placeholder', 'wrong password provided, confirm your password again')
-            document.getElementById('confirmedPassword').classList.add('error');
-        } else if (userdetails.email !== userdetails.confirmedEmail) {
-            setUserdetails({ ...userdetails, confirmedEmail: '' })
-            document.getElementById('confirmedEmail').setAttribute('placeholder', 'wrong email-id provided, confirm your email again')
-            document.getElementById('confirmedEmail').classList.add('error');
-        } else if (userdetails.password !== userdetails.confirmedPassword) {
-            setUserdetails({ ...userdetails, confirmedPassword: '' })
-            document.getElementById('confirmedPassword').setAttribute('placeholder', 'wrong password provided, confirm your password again')
-            document.getElementById('confirmedPassword').classList.add('error');
-        }
-        else {
-            IssignupConfirmed(true);
-            setuserSignedin(true);
-        }
-    }
+const Signup = ({ HandleSubmitforSignupForm, setUserdetails, userdetails, openIntroAndIconInvisible, iconVisible }) => {
     return (
-        <div>
-            {signupconfirmation ? <Login userdetails={userdetails} userSignedin={userSignedin} /> : <form onSubmit={HandleSubmit} id='signupform'>
+        <div className='SignUp'>
+            <div className='intro'>
+                {iconVisible && <div id='show-intro' onClick={() => openIntroAndIconInvisible()}>
+                    <i className="ri-arrow-left-double-line"></i>
+                </div>}
+            </div>
+            <form onSubmit={(e) => HandleSubmitforSignupForm(e)} id='signupform'>
                 <label htmlFor='text'>First Name</label>
                 <input type="text" id='fname' placeholder='type your first name' onChange={(e) => setUserdetails({ ...userdetails, fname: e.target.value })} />
                 <label htmlFor='text'>Last Name</label>
@@ -59,7 +23,7 @@ const Signup = () => {
                 <label htmlFor='password'>Confirm your Password</label>
                 <input type="password" id='confirmedPassword' placeholder='confirm your password' value={userdetails.confirmedPassword} onChange={(e) => setUserdetails({ ...userdetails, confirmedPassword: e.target.value })} />
                 <button type='submit' className='btn btn-block signinbutton'>SignIn</button>
-            </form>}
+            </form>
         </div>
     )
 }
