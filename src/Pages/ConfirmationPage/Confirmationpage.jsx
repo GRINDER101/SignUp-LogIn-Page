f3020ad5f91cb44cacdbf3616680fa8f51ee12bb
Rittik Saha <89324459+GRINDER101@users.noreply.github.com>
2024-07-02 20:51:31 +0100

    Update Confirmationpage.jsx

--- src/Pages/ConfirmationPage/Confirmationpage.jsx
+++ src/Pages/ConfirmationPage/Confirmationpage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./confirmationpage.css"
 
-const Confirmationpage = ({ userdetails }) => {
+const Confirmationpage = ({ userdetails, openIntroAndIconInvisible, iconVisible }) => {
     return (
         <div className='confirmation-container'>
+            <div className='intro-1'>
+                {iconVisible && <div id='show-intro-1' onClick={() => openIntroAndIconInvisible()}>
+                    <i className="ri-arrow-left-double-line"></i>
+                </div>}
+            </div>
             <h3> Thank you {userdetails.fname}, we are glad to know that you liked our product and service and registered with us. You will always be part of our New Tech Consultancy Group.</h3>
         </div>
     )
